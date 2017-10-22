3bb2ca547944a98e11f4448b10fb1922809850b6
Kara Bayse <karabayse@gmail.com>
2017-10-22 16:40:15 -0500

    added conditional with && operator

--- client.js
+++ client.js
@@ -14,6 +14,18 @@ class PropsDisplayer extends React.Component {
   }
 }
 
+
+// JSX Conditionals: &&
+const tasty = (
+  <ul>
+    <li>Applesauce</li>
+    { !baby && <li>Pizza</li> }
+    { age > 15 && <li>Brussels Sprouts</li> }
+    { age > 20 && <li>Oysters</li> }
+    { age > 25 && <li>Grappa</li> }
+  </ul>
+);
+
 // ReactDOM.render goes here:
 ReactDOM.render(
   <PropsDisplayer />,
