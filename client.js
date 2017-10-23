aed057de86d9b4840d002637742f672de1ee09be
Kara Bayse <karabayse@gmail.com>
2017-10-23 15:00:18 -0500

    added red panda

--- client.js
+++ client.js
@@ -31,3 +31,30 @@ ReactDOM.render(
   <PropsDisplayer />,
   document.getElementById('app')
 );
+
+
+
+const redPanda = {
+  src: 'http://bit.ly/1U92LL3',
+  alt: 'Red Panda',
+  width:  '200px'
+};
+
+class RedPanda extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Cute Red Panda</h1>
+        <img
+          src={redPanda.src}
+          alt={redPanda.alt}
+          width={redPanda.width} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <RedPanda />,
+  document.getElementById('app')
+);
